e4d4b376ee70ce0cb561dcf1064a472c2c97c41b
juanvalle <ing.juanlvalle@gmail.com>
2020-01-10 16:23:15 -0800

    Fuzzy_clustering
    Programa para crear un cluster difuso utilizando los objetivos SLL y FNBW de las soluciones que componen un frente de Pareto.

--- fuzzy_clustering.r
+++ fuzzy_clustering.r
@@ -1,10 +1,8 @@
-setwd("~/Documents/scripts/R/antenna/paretos_clust")
-
 library(cluster)
 library(factoextra)
 library(e1071)
 
-family <- read.csv("pareto10-0mod_clust.csv")
+family <- read.csv("pareto10-0mod_clust.csv") # Frente de Pareto en formato CSV
 cm <- cmeans(family, 3, iter.max = 50000, m = 2, method="cmeans", verbose=TRUE)
 fviz_cluster(list(data = family, cluster=cm$cluster), 
                ellipse.type = "norm", ellipse.level = 0.8,
